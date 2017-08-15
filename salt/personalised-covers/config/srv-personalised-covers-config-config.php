0a5b61da73db6c3b425acd3f30f4afdd990fcd22
Gary Tierney <gary.tierney@digirati.com>
2017-08-15 13:12:33 +0100

    Add new cover_images configuration key

--- salt/personalised-covers/config/srv-personalised-covers-config-config.php
+++ salt/personalised-covers/config/srv-personalised-covers-config-config.php
@@ -10,5 +10,11 @@ return [
 	'aws_bucket' => "{{pillar.personalised_covers.aws.bucket}}",
 	'elife_api' => "{{ pillar.personalised_covers.api }}",
 	'landing_page_url' => "{{pillar.personalised_covers.landing_page}}",
+	'cover_images' => [
+		'load_paths' => [
+			'a4' => '/srv/personalised-covers-data/formats/a4',
+			'letter' => '/srv/personalised-covers-data/formats/letter'
+		],
+	],
 ];
 
